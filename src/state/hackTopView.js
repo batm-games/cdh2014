355bbcd13ce679ab3df28d081169912fda5fc301
Gabriel Rea <gabriel.rea@tierconnect.com>
2014-08-01 11:28:14 -0400

    Adding characters folder

--- src/state/hackTopView.js
+++ src/state/hackTopView.js
@@ -3,7 +3,11 @@ function State() {
 
 State.prototype = {
   preload: function () {
-    
+    game.load.image('tileset', './images/tileset.png');
+    game.load.tilemap('map', './images/map_topview.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.spritesheet('dog', './images/sprites/baddie.png', 32, 32);
+
+    game.load.atlas('atlaszebra', './images/spritesheets/atlaszebra.png', './images/spritesheets/atlaszebra.json');    
   },
   create: function () {
     
