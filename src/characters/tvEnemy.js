cf5a6b332939d2ce82bf40bc63287363750fb26f
Alvaro Rojas <alvarok19@gmail.com>
2014-08-01 17:18:14 -0400

    Fixing creating enemies

--- src/characters/tvEnemy.js
+++ src/characters/tvEnemy.js
@@ -1,16 +1,19 @@
 /**
  * Created by user on 8/1/14.
  */
-var TVEnemy = {
-    ENEMY_SPEED :20
+var TVEnemy = function(){
+    this.ENEMY_SPEED = 10;
+    this.enemy = null;
+    this.id = parseInt(Math.random() * 10000);
 };
 
-TVEnemy.createEnemy = function(x, y, key, frame) {
+TVEnemy.prototype.createEnemy = function(x, y, key, frame, enemy_speed) {
     this.X = x;
     this.Y = y;
+    this.ENEMY_SPEED = enemy_speed;
 
     var enemy = this.enemy = game.add.sprite(this.X, this.Y, key, frame);
-    enemy.animations.add('walk', ['walk-1.png','walk-2.png'], 10, true);
+    enemy.animations.add('walk', ['walk-1.png','walk-2.pn'], 10, true);
     enemy.animations.add('stop', ['stop-1.png','stop-2.png'], 10, true);
     enemy.animations.play('walk');
     enemy.anchor.set(0.5,0.5);
@@ -19,13 +22,12 @@ TVEnemy.createEnemy = function(x, y, key, frame) {
     enemy.body.collideWorldBounds = true;
 }
 
-TVEnemy.updateEnemy = function() {
+TVEnemy.prototype.updateEnemy = function() {
     var dir = new Phaser.Point(0,0);
     dir.x += 1;
     dir.setMagnitude(this.ENEMY_SPEED);
     this.enemy.body.velocity.x = dir.x;
     if(this.enemy.body.onWall()) {
-        console.log('wall');
         this.enemy.position.x = 0;
     }
 
