cf79e032717c6bbaa970835fa688443c75835f64
Edwin Guzman <vudduu@gmail.com>
2014-08-02 11:50:56 -0400

    added cebron

--- src/utils/ExtraUtils.js
+++ src/utils/ExtraUtils.js
@@ -1,17 +1,31 @@
 var ExtraUtils = {};
+var imageFlag = false;
 
 ExtraUtils.showCebron = function(game, x, y){
+    if(imageFlag) return;
+    imageFlag = true;
+
     var sprite = game.add.sprite(x, y, "cebron");
-
-    var tween = game.add.tween(sprite)
+    var tween1 = game.add.tween(sprite)
         .to({
-                x: game.camera.x + halfX,
-                y: game.camera.y + halfY
-            }, 400, Phaser.Easing.Sinusoidal.InOut, false, 0, 1, false)
+            x: game.camera.x + halfX,
+            y: game.camera.y + halfY
+        }, 400, Phaser.Easing.Sinusoidal.InOut, false, 0, 0, false)
         .start();
+    console.log("test asdf asf");
 
-    tween.onComplete.add(function() {
-        sprite.kill();
+    tween1.onComplete.add(function () {
+        var tween2 = game.add.tween(sprite)
+            .to({
+                x: x,
+                y: y
+            }, 400, Phaser.Easing.Sinusoidal.InOut, false, 600, 0, false)
+            .start();
+
+        tween2.onComplete.add(function() {
+            sprite.kill();
+            imageFlag = false;
+        });
     }, this);
 };
 
