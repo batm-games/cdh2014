8183f9dfc6b89f77b0f2486dbd50552c04f3489c
Alvaro Rojas <alvarok19@gmail.com>
2014-08-02 01:48:09 -0400

    Creating Ghot enemies

--- src/characters/tvEnemy.js
+++ src/characters/tvEnemy.js
@@ -5,11 +5,11 @@ var TVEnemy = {
   createEnemy: function(x, y, key, frame, enemySpeed) {
     this.enemySpeed = enemySpeed;
     var enemy = game.add.sprite(x,y, key, frame);
-    enemy.animations.add('walk', [0, 1, 2, 3, 4], 10, true);
+    enemy.animations.add('walk', [4, 3, 2, 1, 0], 10, true);
     // enemy.animations.add('stop', ['stop-1.png','stop-2.png'], 10, true);
     enemy.animations.play('walk');
     enemy.anchor.set(0.5,0.5);
-//    enemy.scale.set(0.25,0.25);
+    enemy.scale.set(0.25,0.25);
     game.physics.arcade.enable(enemy);
     enemy.body.collideWorldBounds = true;
     enemy.body.velocity.x = enemySpeed;
@@ -19,7 +19,9 @@ var TVEnemy = {
   },
   createGhost: function(x, y, enemySpeed) {
     var KEY = 'ghost';
-    return this.createEnemy(x, y, KEY, null, enemySpeed);
+    var enemy = this.createEnemy(x, y, KEY, null, enemySpeed);
+    enemy.scale.set(1, 1);
+    return enemy;
   },
   updateEnemy: function(enemy) {
     if(enemy.body.onWall()) {
