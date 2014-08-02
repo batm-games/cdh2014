db1b2146471fec2ca025c4142f3d657ed451daf0
Gabriel Rea <gabriel.rea@tierconnect.com>
2014-08-02 13:33:00 -0400

    Creditos

--- src/state/credits.js
+++ src/state/credits.js
@@ -6,7 +6,19 @@ State.prototype = {
     
   },
   create: function () {
-    var text = "You Win!\nCredits\n";
+    var text = "Ganasteeeeeeeee!\n";
+    text += "Creditos\n";
+    text += "== Diseñador Grafico == \nMiguel Velasco\n";
+    text += "== Programadores ==\n";
+    text += "Mauricio Poppe\n";
+    text += "Edwin Guzman\n";
+    text += "Thaer Latif\n";
+    text += "Ronald Valverde\n";
+    text += "Alvaro Rojas\n";
+    text += "Gabriel Rea\n\n\n";
+    text += "Bitless Against the Machine\n\n";
+    text += "Gracias por jugar\n";
+
         var style = { font: "65px Arial", fill: "#00ff00", align: "center" };
 
 //        var lightMask = new LightMask(this.fire);
@@ -15,7 +27,13 @@ State.prototype = {
 //            ,15000
 //        );
 
-    var t = game.add.text(X*0.5,Y*0.5, text, style);
+    var t = game.add.text(X*0.5,Y*1.7, text, style);
+    t.anchor.set(0.5,0.5);
+
+    game.add.tween(t)
+    .to({y:0},15000)
+    .start();
+
     StateUtils.createDemoHomeButton();
   },
   update: function (event) {
